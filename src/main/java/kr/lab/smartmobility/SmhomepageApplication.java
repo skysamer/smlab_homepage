ea31847e1172b870d54cbc1606e7072f0bd223a3
skysamer <skyslayer123@naver.com>
2021-10-13 21:27:53 +0900

    Initial commit

---
+++ src/main/java/kr/lab/smartmobility/SmhomepageApplication.java
@@ -0,0 +1,13 @@
+package kr.lab.smartmobility;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmhomepageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmhomepageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/lab/smartmobility/SmhomepageApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.lab.smartmobility;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmhomepageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
